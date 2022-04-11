1a47265dfd511aa055f8bf6794c1bbebdb1d8da4
Anjan Lamichhane <98454@Anjan.Anjan>
2022-04-11 15:20:27 +0545

    'message'

--- file.cpp
+++ file.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class demo{
+    private:
+        int a ,b ;
+    public:
+        void getdata();
+        void putdata();
+
+
+};
+
+ void demo::getdata(){
+     cout<<"enter the first number"<<endl;
+     cin>>a;
+     cout<<"enter the second number"<<endl;
+     cin>>b;
+ }
+
+ void demo::putdata(){
+     cout<<"the entered first and second number are :"<<a<<b<<endl;
+ }
+
 int main()
 {
     cout << "hello there" << endl;
+
+    
 
     return 0;
 }
